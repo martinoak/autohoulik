76e34617973c86f6b1b8d8e849e16321554ae93c
Martin Dub <martin.dub@dek-cz.com>
2026-01-05 10:40:17 +0100

    AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forpsi MySQL rule
+        Schema::defaultStringLength(191);
+
+        // Vite override
+        Vite::prefetch(concurrency: 3);
     }
 }
